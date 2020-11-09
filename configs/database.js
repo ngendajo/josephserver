8bac356ce7e7d1fe9a52bc1da6a2c3cbb8cb9b97
teacher <teacher@ASYV-IT-LBPC050.lnfhs.asyv.org>
2020-11-09 14:41:39 +0200

    database error

--- configs/database.js
+++ configs/database.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
-
-mongoose
+try{
+  mongoose
   .connect("mongodb+srv://ngendajo:ngendajo@cluster0.jctao.mongodb.net/josephdb?retryWrites=true&w=majority", {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(console.log("Connected to the database")); 
+}
+catch(e)
+{
+  console.log(e);
+}
